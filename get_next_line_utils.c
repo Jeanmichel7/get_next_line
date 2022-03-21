09ff4ee10fa09c4ebbb8d901d58d7104913a445c
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2022-03-21 22:25:09 +0100

    fin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: jrasser <jrasser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 15:29:07 by jrasser           #+#    #+#             */
-/*   Updated: 2022/03/21 22:13:53 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/03/21 22:24:11 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,11 @@ char	*update_buffer(char *buffer)
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
 	if (i == BUFFER_SIZE)
+	{
 		free(buffer);
-	else
-		buffer = buffer + i + 1;
+		return (NULL);
+	}
+	buffer = buffer + i + 1;
 	return (buffer);
 }
 
